11e399907d5b6791d8a13f6056057f7f4897c657
Xalenz <112818717+darkzdark@users.noreply.github.com>
2026-05-25 13:54:20 +0700

    Add files via upload

---
+++ ImageEnhancer/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "ImageEnhancer"
+include(":app")
+// Setelah import OpenCV module, uncomment baris di bawah:
+// include(":opencv")
